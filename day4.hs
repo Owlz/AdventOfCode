277c01ff0ed5742fe1d1f83030cea5525171dda5
Owlz <urban97-luca@hotmail.it>
2019-12-06 14:04:28 +0100

    Solved Day4

--- day4.hs
+++ day4.hs
@@ -6,20 +6,21 @@ main :: IO ()
 main = 
   let input = "372304-847060"
       solution1 = (show . solve  . parse) input 
---    solution2 = (show . solve' . parse) input
+      solution2 = (show . solve' . parse) input
   in
-  putStrLn $ "Solution 1: " ++ solution1
---putStrLn $ "Solution 2: " ++ solution2
-
+    putStrLn $ "Solution 1: " ++ solution1 ++ "\nSolution 2: " ++ solution2
     
 solve :: [Int] -> Int
-solve bs = length . filter isRuleCompliant $ map show [bs !! 0..bs !! 1] 
+solve bs = length . filter (\x -> length x == 6 && hasAtLeastDouble x && isIncreasing x) $ map show [bs !! 0..bs !! 1] 
 
-isRuleCompliant :: String -> Bool
-isRuleCompliant x = length x == 6 && isIncreasing x && hasDouble x
+solve' :: [Int] -> Int
+solve' bs = length . filter (\x -> length x == 6 && hasDouble x && isIncreasing x) $ map show [bs !! 0..bs !! 1] 
 
 hasDouble :: String -> Bool
-hasDouble s = any (\x -> length x == 2) $ groupBy (\x y -> x == y) s
+hasDouble s = any (\x -> length x == 2) $ group s
+
+hasAtLeastDouble :: String -> Bool
+hasAtLeastDouble s = any (\x -> length x >= 2) $ group s
 
 isIncreasing :: String -> Bool
 isIncreasing (x:y:xs) = if(digitToInt x <= digitToInt y) then isIncreasing $ y:xs else False
